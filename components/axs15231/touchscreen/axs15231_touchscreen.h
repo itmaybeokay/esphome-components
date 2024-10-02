340e92744165649eed137ce8b3f687b886de921a
itmaybeokay <jon.hanford@gmail.com>
2024-10-02 11:13:48 -0600

    Update axs15231_touchscreen.h
    Attempt Interrupt Pin headers

--- components/axs15231/touchscreen/axs15231_touchscreen.h
+++ components/axs15231/touchscreen/axs15231_touchscreen.h
@@ -16,11 +16,17 @@ class AXS15231Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevi
   void set_reset_pin(GPIOPin *pin) {
     this->reset_pin_ = pin;
   }
+  //Set Interrupt Pin via ESPhome on Internal GPIO Pin, no expanders
+  void set_interrupt_pin(InternalGPIOPin *interrupt_pin) { 
+    this->interrupt_pin_ = interrupt_pin; 
+  }
 
  protected:
   void update_touches() override;
 
   GPIOPin *reset_pin_{};
+  // defualt to nothing I guess?
+  InternalGPIOPin *interrupt_pin_{nullptr};
 };
 
 }  // namespace axs15231
