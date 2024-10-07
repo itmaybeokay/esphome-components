16fcdebaa3aefd6be67593a22c64929425125364
itmaybeokay <jon.hanford@gmail.com>
2024-10-07 11:56:13 -0600

    Update axs15231_touchscreen.cpp
    Try again...

--- components/axs15231/touchscreen/axs15231_touchscreen.cpp
+++ components/axs15231/touchscreen/axs15231_touchscreen.cpp
@@ -3,33 +3,30 @@
 #include "esphome/core/gpio.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
-#include "esphome/components/i2c/i2c.h"  // Include I2C header
+#include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
 namespace axs15231 {
 
 namespace {
   constexpr static const char *const TAG = "axs15231.touchscreen";
-  constexpr static const uint8_t AXS_READ_TOUCHPAD[11] = {0xb5, 0xab, 0xa5, 0x5a, 0x0, 0x0, 0x0, 0x8};
+  constexpr static const uint8_t AXS_READ_TOUCHPAD[8] = {0xb5, 0xab, 0xa5, 0x5a, 0x00, 0x00, 0x00, 0x08};
 } // anonymous namespace
 
-// Manufacturer-provided functions
 bool WriteC8D8(uint8_t c, uint8_t d, esphome::i2c::I2CDevice *device) {
-  if (device->write_byte(c) != i2c::ERROR_OK) {
-    ESP_LOGE(TAG, "->Write(c=0x%02X) fail", c);
-    return false;
-  }
-  if (device->write_byte(d) != i2c::ERROR_OK) {
-    ESP_LOGE(TAG, "->Write(d=0x%02X) fail", d);
+  uint8_t data[2] = {c, d};
+  auto err = device->write(data, 2, true);
+  if (err != esphome::i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Failed to write bytes c=0x%02X, d=0x%02X", c, d);
     return false;
   }
   return true;
 }
 
 bool IIC_WriteC8D8(uint8_t device_address, uint8_t c, uint8_t d, esphome::i2c::I2CDevice *device) {
-  device->set_address(device_address);
+  device->set_i2c_address(device_address);
   if (!WriteC8D8(c, d, device)) {
-    ESP_LOGE(TAG, "->WriteC8D8(c=0x%02X, d=0x%02X) fail", c, d);
+    ESP_LOGE(TAG, "Failed to write to device 0x%02X", device_address);
     return false;
   }
   return true;
@@ -58,7 +55,7 @@ void AXS15231Touchscreen::setup() {
   this->y_raw_max_ = this->display_->get_height();
 
   // Set the I2C device address
-  this->set_address(0x6A);
+  this->set_i2c_address(0x6A);
 
   // Disable ILIM
   if (!IIC_WriteC8D8(0x6A, 0x00, 0b00111111, this)) {
@@ -84,10 +81,16 @@ void AXS15231Touchscreen::update_touches() {
   uint16_t w;
 
   err = this->write(AXS_READ_TOUCHPAD, sizeof(AXS_READ_TOUCHPAD), false);
-  I2C_ERROR_CHECK(err);
+  if (err != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Failed to write touchpad read command");
+    return;
+  }
 
   err = this->read(buff, AXS_TOUCH_DATA_SIZE);
-  I2C_ERROR_CHECK(err);
+  if (err != i2c::ERROR_OK) {
+    ESP_LOGE(TAG, "Failed to read touch data");
+    return;
+  }
 
   x = AXS_GET_POINT_X(buff, 0);
   y = AXS_GET_POINT_Y(buff, 0);
@@ -98,7 +101,7 @@ void AXS15231Touchscreen::update_touches() {
   } else {
     ESP_LOGI(TAG, "Touch Weight: %d", w);
   }
-  if (w > 130) { // attempt to filter touch events below weight threshold.
+  if (w > 130) {
     return;
   }
   this->add_raw_touch_position_(0, x, y);
