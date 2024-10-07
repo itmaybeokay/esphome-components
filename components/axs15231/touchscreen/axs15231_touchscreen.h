63eb9008c0e96fd245d4b950985909fa5d5862c7
itmaybeokay <jon.hanford@gmail.com>
2024-10-07 11:30:31 -0600

    Update axs15231_touchscreen.h
    dev -> main

--- components/axs15231/touchscreen/axs15231_touchscreen.h
+++ components/axs15231/touchscreen/axs15231_touchscreen.h
@@ -4,6 +4,7 @@
 #include "esphome/components/touchscreen/touchscreen.h"
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace axs15231 {
@@ -16,11 +17,17 @@ class AXS15231Touchscreen : public touchscreen::Touchscreen, public i2c::I2CDevi
   void set_reset_pin(GPIOPin *pin) {
     this->reset_pin_ = pin;
   }
+  //Set Interrupt Pin via ESPhome on Internal GPIO Pin, no expanders
+  void set_interrupt_pin(InternalGPIOPin *interrupt_pin) { 
+    this->interrupt_pin_ = interrupt_pin; 
+  }
 
  protected:
   void update_touches() override;
 
   GPIOPin *reset_pin_{};
+  // defualt to nothing I guess?
+  InternalGPIOPin *interrupt_pin_{nullptr};
 };
 
 }  // namespace axs15231
